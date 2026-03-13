b7012ec946652f6010ecec31924cc6b47c830f84
ISMAIL <139564142+isacks456@users.noreply.github.com>
2026-03-13 14:14:34 +0300

    Initialize 2D array in fileName.cpp
    Define a 2D array with specified dimensions and values.

---
+++ fileName.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+void main() {
+
+
+	setlocale(LC_ALL, "RU");
+
+	const int ROW = 3;
+	const int COL = 4;
+	
+	int arr[ROW][COL] = 
+	{
+		{2,4,55,1},
+		{4,2,4,5}
+	};
+
+	
+
+
+	
+	
+}
